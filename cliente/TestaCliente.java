ad72200025419fad660991c55157386ad0afb842
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-11-03 16:11:58 -0300

    A ultima correcao

--- cliente/TestaCliente.java
+++ cliente/TestaCliente.java
@@ -6,7 +6,9 @@ public class TestaCliente {
 
     public static void main(String[] args) {
         Cliente c1 = new Cliente("Filipe", 123, "706.640.532-18", "(11) 97744-3333", "08690-732" );
+        Cliente c2 = new Cliente("Armando", 456, "706.640.532-16", "(11) 97744-4444", "08690-732" );
 
         c1.visualizar();
+        c2.visualizar();
     }
 }
