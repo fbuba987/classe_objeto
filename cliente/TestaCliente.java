c3e4e15d73b451019705a5ec7ad56d2495b9e2e0
“fbuba987” <115090534+fbuba987@users.noreply.github.com>
2022-11-04 16:44:03 -0300

    Corrigindo um erro

--- cliente/TestaCliente.java
+++ cliente/TestaCliente.java
@@ -8,8 +8,13 @@ public class TestaCliente {
 
     public static void main(String[] args) {
         PessoaFisica p1 = new PessoaFisica("Filipe", 123, "Sao Paulo", 1, "45700000", "708.453.654-19");
-        PessoaJuridica pj1 = new PessoaJuridica("Armando", 321, "Mala", 2, "43900000", "808.453.654-19");
+        PessoaFisica p2 = new PessoaFisica("Buba", 456, "Porto", 1, "42300000", "705.653.654-19");
+        PessoaJuridica pj1 = new PessoaJuridica("Armando", 321, "Mala", 2, "43900000", "808.453/654-19");
+        PessoaJuridica pj2 = new PessoaJuridica("Matu", 325, "Santo", 2, "43900000", "808.453/654-18");
+
         p1.visualizar();
+        p2.visualizar();
         pj1.visualizar();
+        pj2.visualizar();
     }
 }
